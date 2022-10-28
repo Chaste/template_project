49a7820c12d97140c9306d64965e2a2380e0ac0f
MILeach <matt-leach@hotmail.co.uk>
2022-10-28 14:01:20 +0100

    Multiple Project Support (#2)
    add support for multiple template projects

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ You now simply click the big green "Use this template" button to use a copy of t
 Alternatively, if you aren't a github user, you can download a zip (see Releases button) and start your own repository with that.
 
 Then see the [User Projects](https://chaste.cs.ox.ac.uk/trac/wiki/ChasteGuides/UserProjects) guide page on the Chaste wiki for more information.
+
+If you clone this repository, you should make sure to rename the template_project folder with your project name and run the 'setup_project.py' script to avoid conflicts if you have multiple projects.
--- setup_project.py
+++ setup_project.py
@@ -31,6 +31,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import os
+from functools import partial
 
 
 def find_and_replace(filename, old_string, new_string):
@@ -67,6 +68,18 @@ def ask_for_response(question):
         ask_for_response("Please respond with yes or no:")
 
 
+# Appends text_to_append before the file extension
+def append_to_file_name(text_to_append, file):
+    new_name = file.replace('.', text_to_append + '.')
+    os.rename(file, new_name)
+    return new_name
+
+
+# Converts a project name to something that will be valid to append to a C++ class name
+def sanitize_project_name(project_name):
+    return ''.join(filter(lambda char: char.isalpha() or char.isnumeric(), project_name))
+
+
 def main():
     # The absolute path to the project directory
     path_to_project = os.path.dirname(os.path.realpath(__file__))
@@ -79,7 +92,39 @@ def main():
     apps_cmakelists = os.path.join(path_to_project, 'apps', 'CMakeLists.txt')
     test_cmakelists = os.path.join(path_to_project, 'test', 'CMakeLists.txt')
 
-    # Perform the find-and-replace tasks to update the template project
+    # Files to append project name to - this avoids conflicts if mutliple projects are generated from the template project
+    files_requiring_append = [os.path.join(path_to_project, 'apps', 'src', 'ExampleApp.cpp'),
+                              os.path.join(path_to_project, 'src', 'Hello.cpp'),
+                              os.path.join(path_to_project, 'src', 'Hello.hpp'),
+                              os.path.join(path_to_project, 'test', 'TestHello.hpp')]
+
+    # Append project name to required files
+    append_project_name = partial(append_to_file_name, '_' + project_name)
+    appended_file_names = list(map(append_project_name, files_requiring_append))
+
+    # Perform the find-and-replace tasks to update the template project source
+    sanitized_name = sanitize_project_name(project_name)
+
+    substitutions = { # These are very specific to avoid rewriting the printed out "Hello world" message
+        " TestHello": " TestHello_" +  sanitized_name,
+        "TestHello.hpp": "TestHello_" + project_name + ".hpp",
+        "HELLO": "HELLO_" + sanitized_name.upper(),
+        "Hello world(": "Hello_" + sanitized_name + " world(",
+        "class Hello": "class Hello_" + sanitized_name,
+        "Hello::": "Hello_" + sanitized_name + "::",
+        "Hello(": "Hello_" + sanitized_name + "(",
+        "TestHello.hpp": "TestHello_" + project_name + ".hpp",
+        "Hello.hpp": "Hello_" + project_name + ".hpp"
+    }
+
+    files_to_sub = appended_file_names + [str(os.path.join(path_to_project, 'test', 'ContinuousTestPack.txt'))]
+
+    for file in files_to_sub:
+        for old, new in substitutions.items():
+            find_and_replace(file, old, new)
+
+
+    # Perform the find-and-replace tasks to update the template project cmake
     find_and_replace(base_cmakelists, 'chaste_do_project(template_project', 'chaste_do_project(' + project_name)
     find_and_replace(apps_cmakelists, 'chaste_do_apps_project(template_project', 'chaste_do_apps_project(' + project_name)
     find_and_replace(test_cmakelists, 'chaste_do_test_project(template_project', 'chaste_do_test_project(' + project_name)
